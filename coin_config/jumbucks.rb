beaa8b5fba72afb31d856c5f557bd0c9e23bff8f
Luke Williams <luke@iquidus.io>
2015-09-30 20:56:18 +1300

    added jumbucks

---
+++ coin_config/jumbucks.rb
@@ -0,0 +1,23 @@
+module Jumbucks
+  PERFORM_ERROR = 'That error doesn\'t exist'
+  BALANCE_REPLY_PRETEXT = 'Your balance: '
+  CURRENCY_ICON = 'JBS'
+  WEALTHY_UPPER_BOUND = 1000
+  WEALTHY_UPPER_BOUND_POSTTEXT = ' whale alert!'
+  WEALTHY_UPPER_BOUND_EMOJI = ':moneybag:'
+  BALANCE_REPLY_POSTTEXT = ' Coins'
+  DEPOSIT_PRETEXT = 'Make a deposit'
+  DEPOSIT_POSTTEXT = 'deposit JBS to this address'
+  TIP_ERROR_TEXT = 'please say tip @username amount'
+  TIP_PRETEXT = 'So generous!'
+  TIP_POSTTEXT1 = 'http://explorer.getjumbucks.com/tx/'
+  TIP_POSTTEXT2 = '|blockchain'
+  WITHDRAW_TEXT = 'You\'re stingy'
+  WITHDRAW_ICON = ':shit:'
+  NETWORKINFO_ICON = ':bar_chart:'
+  TOO_POOR_TEXT = 'Sorry, you don\'t have enough JBS.'
+  NO_PURPOSE_LOWER_BOUND_TEXT = 'Too small. No one wants your dust.'
+  NO_PURPOSE_LOWER_BOUND = 1
+  RANDOMIZED_EMOJI = ':black_joker:'
+  NETWORK = 'jumbucks'
+end
